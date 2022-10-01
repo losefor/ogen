032194cdd1e56626b81fb726f3106ff7d946dcfc
Muhammed Baqer Ahmed <moh999157@gmail.com>
2022-10-01 05:43:46 +0300

    ✨: setup a dev server for react

--- rollup.config.js
+++ rollup.config.js
@@ -21,11 +21,81 @@ if (!pages.length) {
   throw new Error(`No sources found in: ${dir}`)
 }
 
-export default pages.map(page => {
+export default commandLineArgs => {
+
+  console.log({ commandLineArgs });
+  const isBundle = commandLineArgs.bundle
+  const devPage = commandLineArgs.x
+
+  // clear args 
+  delete commandLineArgs.bundle
+  delete commandLineArgs.x
+
+
+  bootstrap({
+    isBundle,
+    devPage
+  })
+
+
+  // Prod config
+  if (isBundle) {
+    return pages.map(page => {
+      return {
+        input: `dist/pages/${page}.js`,
+        output: {
+          file: `dist/bundle/${page}.js`,
+          format: "iife",
+          sourcemap: true,
+        },
+        plugins: [
+          resolve({
+            preferBuiltins: true
+          }),
+          babel({
+            exclude: "node_modules/**",
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }),
+          commonjs({
+            namedExports: {
+              "react-dom": Object.keys(ReactDOM),
+              react: Object.keys(React)
+            }
+          }),
+          replace({
+            "process.env.NODE_ENV": JSON.stringify("production"),
+            preventAssignment: true
+          }),
+          globals(),
+          builtins(),
+          !isBundle ? serve({
+            open: true,
+            verbose: true,
+            contentBase: ["", "public"],
+            host: "localhost",
+            port: 3000,
+          }) : null,
+          !isBundle ? livereload({ watch: "dist" }) : null,
+        ]
+      }
+    })
+  }
+
+  // Dev config
+  // if nod dev page provided
+  if (devPage === true) {
+    throw new Error("Please provide a page to watch")
+  }
+
+  // if the page is not exist 
+  if (!pages.includes(devPage)) {
+    throw new Error("This page seems to be not exist. Please provide a valid page to watch")
+  }
+
   return {
-    input: `dist/pages/${page}.js`,
+    input: `dist/pages/${devPage}.js`,
     output: {
-      file: `dist/bundle/${page}.js`,
+      file: `dist/bundle/${devPage}.js`,
       format: "iife",
       sourcemap: true,
     },
@@ -49,21 +119,29 @@ export default pages.map(page => {
       }),
       globals(),
       builtins(),
-      // serve({
-      //   open: true,
-      //   verbose: true,
-      //   contentBase: ["", "public"],
-      //   host: "localhost",
-      //   port: 3000,
-      // }),
-      // livereload({ watch: "dist" }),
+      serve({
+        open: true,
+        verbose: true,
+        contentBase: ["dist"],
+        host: "localhost",
+        port: 3000,
+      }),
+      livereload({ watch: "dist" }),
     ]
   }
-})
+
+
+
+
+}
+
 
 // Bootstrap index.html
-fs.writeFileSync("dist/index.html",
-  `<!DOCTYPE html>
+const bootstrap = (props) => {
+
+  if (props.isBundle) {
+    fs.writeFileSync("dist/index.html",
+      `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
@@ -75,5 +153,25 @@ fs.writeFileSync("dist/index.html",
     <div id="root"></div>
   </body>
 </html>`)
+  } else {
+    fs.writeFileSync("dist/index.html",
+      `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>React - Rollup Test</title>
+</head>
+<body>
+  <noscript> You need to enable JavaScript to run this app. </noscript>
+  <div id="root"></div>
+  <script src="./bundle/${props.devPage}.js" ></script>
+</body>
+</html>`)
+  }
+
+}
+
+
 
 
